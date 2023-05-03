4c19558badb581301882671c4e01d469f0375e31
zack <zack.bitcoin@gmail.com>
2023-05-03 11:15:39 -0400

    fix typo

--- src/dump.erl
+++ src/dump.erl
@@ -34,7 +34,7 @@ terminate(_, _) ->
     ok.
 handle_info(_, X) -> {noreply, X}.
 handle_cast(delete_all, {hd, _, W, ID, Loc}) -> 
-    bits:reset(),
+    bits:reset(ID),
     {norepy, {hd, 1, W, ID, Loc}};
 handle_cast(delete_all, {ram, _, W, ID, Loc}) -> 
     ets:delete_all_objects(ID),
